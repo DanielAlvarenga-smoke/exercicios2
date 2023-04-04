299d522b2e397f67a44bdc0ed776e249174fbd6e
DanielAlvarenga-smoke <red57sw2@gmail.com>
2023-04-03 21:23:39 -0300

    Aula 03

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,22 @@
 import React, {Component} from "react";
 import { View, Text, StyleSheet } from "react-native";
 import Simples from "./componentes/Simples";
-
+import Frag from "./componentes/Frag";
+import ParImpar from "./componentes/ParImpar";
+import Familia from "./componentes/relacao/Familia";
+import Membro from "./componentes/relacao/Membro";
 
 export default class App extends Component {
   render() {
     return(
       <View style = {estilos.container}>
-      <Simples />
+      {/*<Simples texto='Daniel'/>*/}
+      {/*<Frag titulo="cadastro" subTitulo="Tela de cadastro de produto" />*/}
+      {/*<ParImpar numero={11}/>*/}
+      <Familia>
+        <Membro nome="Ana" sobreNome="Silva"/>
+        <Membro nome="João" sobreNome="Silva"/>
+      </Familia>
       </View>
     );
 
--- src/componentes/Simples.tsx
+++ src/componentes/Simples.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Text } from "react-native";
+import Padrao from "../estilo/Padrao";
 
-export default function(){
-    return <Text>Simples!</Text>
-}
+export default (props: any ) => 
+ <Text style={Padrao.ex} > Arrow: 1 {props.Text}</Text>
